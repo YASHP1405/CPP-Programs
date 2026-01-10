b14e122eb237109447e4b8103a7af0a2e053d528
Yash Uttam Pawar <yupawar689@gmail.com>
2026-01-10 16:25:57 +0530

    CPP STARTED

---
+++ CPP.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i;
+    cout<<"Enter a number: ";
+    cin>>n;
+    for (i=1;i<11;i++){
+        cout   << n << " x " << i << " = " << n * i << endl;
+    }
+    return 0;
+}
