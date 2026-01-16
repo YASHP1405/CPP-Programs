3bd921203d2c330f184405a367cb27dac8ca7e26
Yash Uttam Pawar <yupawar689@gmail.com>
2026-01-16 18:56:08 +0530

    Question practise from class

--- Basics of CPP/evenodd.cpp
+++ Basics of CPP/evenodd.cpp
@@ -1,5 +1,5 @@
 /* 7. Write a program to check whether a number is even or odd. */
-
+/*
  #include<iostream>
  using namespace std;
  void evenodd(int num);
@@ -18,3 +18,22 @@
      else
          cout << num << " is odd." << endl;
  }
+ */
+
+ #include<iostream>
+ using namespace std;
+ int  evenodd(int num);
+
+ int main() {
+     int num;
+     cout << "Enter an integer: ";
+     cin >> num;
+     if(evenodd(num))
+            cout << num << " is even." << endl;
+        else
+            cout << num << " is odd." << endl;
+     return 0;
+ }
+    int evenodd(int num) {
+        return num % 2 == 0;
+    }
