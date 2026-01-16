74fed64be99e4e4884d3b4f0bba15655c31a5a04
Yash Uttam Pawar <yupawar689@gmail.com>
2026-01-16 21:07:16 +0530

    Question practise from class

---
+++ Basics of CPP/multi of 3.cpp
@@ -0,0 +1,25 @@
+/*14. Write a program to check whether a number is a multiple of 3.
+ */
+
+#include <iostream>
+using namespace std;
+
+void checkmultiple(int n);   // function declaration
+
+int main() {
+    int n;
+    cout << "Enter the number to check whether it is multiple of 3 or not: ";
+    cin >> n;
+
+    checkmultiple(n);   // function call
+    return 0;
+}
+
+void checkmultiple(int n) {
+    if (n % 3 == 0) {
+        cout << "It is a multiple of 3: " << n << endl;
+    }
+    else {
+        cout << "It is not a multiple of 3" << endl;
+    }
+}
