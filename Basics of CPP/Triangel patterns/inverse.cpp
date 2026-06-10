7c4ab2df3c29fd0335052b1eabe0e0af60b785e3
Yash Uttam Pawar <yupawar689@gmail.com>
2026-06-10 23:36:22 +0530

    Triangle updates

--- Basics of CPP/Triangel patterns/inverse.cpp
+++ Basics of CPP/Triangel patterns/inverse.cpp
@@ -1,14 +1,21 @@
-// crate a inverse triangle pattern using nested loop
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int i,j;
-    for (i=5; i>=1; i--){
-        for (j=1; j<=i; j++){
-            cout<<"* ";
-        }
-        cout<<endl;
-    }
-    return 0;
 
+int main() {
+    int i, j;
+
+    for(i = 5; i >= 1; i--) {
+
+        for(j = 1; j <= 5 - i; j++) {
+            cout << " ";
+        }
+
+        for(j = 1; j <= i; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
 }
