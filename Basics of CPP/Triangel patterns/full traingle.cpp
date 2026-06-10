f51abd1b1cd95d80fcd9fb330628ebdec3dd0b28
Yash Uttam Pawar <yupawar689@gmail.com>
2026-06-10 23:40:30 +0530

    Reviosn triangle

--- Basics of CPP/Triangel patterns/full traingle.cpp
+++ Basics of CPP/Triangel patterns/full traingle.cpp
@@ -1,14 +1,19 @@
+// full triangle pattern
 #include<iostream>
 using namespace std;
 
 int main(){
     int i,j;
-
     for (i=1; i<=5; i++){
-        for (j=1; j<=i; j++){
+        for (j=1; j<=5-i; j++){
+            cout<<" ";
+        }
+        for(j=1; j<=i; j++){
             cout<<"* ";
         }
         cout<<endl;
     }
-        return 0;
-        }
+    return 0;
+}
+
+
